06a4624a9daddd1eb3ff13807655a04c2ab03f67
Demonity11 <xxdm_11@outlook.com>
2024-09-30 08:16:51 -0300

    Pseudocode written.

---
+++ code.js
@@ -0,0 +1,11 @@
+// This section contains the pseudocode
+// The user will give a prompt which can be rock, paper or scissors
+// The computer will randomly pick rock, paper or scissors too
+// After that the values given will be compared
+// IF the player pick "Rock" and the computer pick "Scissors" then the player wins
+// IF the player pick "Rock" and the computer pick "Paper" then the player loses
+// IF the player pick "Rock" and the computer pick "Rock" then there's a tie
+// IF the player pick "Scissors" and the computer pick "Scissors" then there's a tie
+// IF the player pick "Scissors" and the computer pick "Paper" then the player wins
+// IF the player pick "Scissors" and the computer pick "Rock" then the player loses
+// And go on...
