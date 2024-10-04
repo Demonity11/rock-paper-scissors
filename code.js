de6584601c0e301b8856e93c351eea0135780c49
Marcelo Esau <xxdm_11@outlook.com>
2024-10-04 07:43:21 -0300

    User ui added.

--- code.js
+++ code.js
@@ -1,16 +1,15 @@
 function main() {
-    let userInput = prompt("Choose rock, paper or scissors: ");
-    if (userInput === null || userInput === "") {
-        return console.log("Player quit.")
-    }
-    userInput = userInput.toLowerCase();
+    const pressedBtn = document.querySelectorAll(".btn");
 
-    const pcInput = Math.floor(Math.random() * 3);
+    pressedBtn.forEach((button) => {
+       button.addEventListener("click", () => {
 
-    game(userInput, pcInput);
-    console.log(`Player: ${user_score} | Computer : ${computer_score}`);
+        let pcInput = Math.floor(Math.random() * 3);
+        let userInput = button.id;
 
-    return main();
+        game(userInput, pcInput);
+       });
+    });
 }
 
 function game(playerInput, computerInput) {
@@ -18,26 +17,27 @@ function game(playerInput, computerInput) {
     let index = computerInput;
     computerInput = possibilities.at(index);
 
+    const log = document.querySelector("#log");
+    const score = document.querySelector("#score");
+
     if (playerInput === computerInput) {
-        console.log("It's a tie!");
+        log.textContent = "It's a tie!";
     } 
     else if ((playerInput === "rock" && computerInput === "scissors") || 
              (playerInput === "paper" && computerInput === "rock") ||
              (playerInput === "scissors" && computerInput === "paper")) {
         user_score++;
-        console.log(`You win! ${playerInput} beats ${computerInput}!`);
+        log.textContent = `You win! ${playerInput} beats ${computerInput}!`;
 
     } 
     else if ((playerInput === "rock" && computerInput === "paper") ||
             (playerInput === "paper" && computerInput === "scissors") ||
             (playerInput === "scissors" && computerInput === "rock")) {
         computer_score++;
-        console.log(`You lose! ${computerInput} beats ${playerInput}!`);
-
-    } 
-    else {
-        console.log("The typed input it's not 'rock', 'paper' or 'scissors'.");
+        log.textContent = `You lose! ${computerInput} beats ${playerInput}!`;
     }
+
+    score.textContent = `Player: ${user_score} | Computer : ${computer_score}`;
 }
 
 let user_score = 0;
